49a7e64ad3105a063194275e202e0c2e94fe1b64
MYEONGJU\mjk04 <kmj042894@gmail.com>
2026-02-22 22:46:15 +0900

    c_study first

---
+++ c/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// compile command
+// PS C:\IT_workspace\programming_skill\c> gcc .\hello_world.c
+// PS C:\IT_workspace\programming_skill\c> .\a.exe
+
+int main(void)
+{
+    printf("Hello World\n");
+
+    return 0;
+}
