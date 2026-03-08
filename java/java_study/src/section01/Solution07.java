4eda1071b51eeba69f49d66d2aaa57d68a263932
MYEONGJU\mjk04 <kmj042894@gmail.com>
2026-03-08 21:22:38 +0900

    java study 20260308

---
+++ java/java_study/src/section01/Solution07.java
@@ -0,0 +1,30 @@
+package section01;
+
+public class Solution07 {
+
+    public static void main(String[] args) {
+        /*
+        * 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 한다.
+        * 문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 아니라면 "NO"를 출력하는 프로그램
+        * 대소문자 구분 X
+        *
+        * input : gooG
+        * output : YES
+        * */
+
+        System.out.println("result : " + solution("gooG"));
+    }
+
+    public static String solution(String str) {
+        str = str.toUpperCase();
+        int len = str.length();
+
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) {
+                return "NO";
+            }
+        }
+
+        return "YES";
+    }
+}
