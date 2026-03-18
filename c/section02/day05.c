e42fcd8dbd39721a98baa185d1d6b21c983fcfbd
MYEONGJU\mjk04 <kmj042894@gmail.com>
2026-03-18 10:31:24 +0900

    c study 20260318

---
+++ c/section02/day05.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/*
+이중반복문 ⭐ 피라미드 만들기
+*/
+
+int main(void)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("======================\n");
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i; j < 5 - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
