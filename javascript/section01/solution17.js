e1b90aa5081e41d8eba548565629eba9a023a565
MYEONGJU\mjk04 <kmj042894@gmail.com>
2026-04-01 07:16:03 +0900

    js study 20260401 2

--- javascript/section01/solution17.js
+++ javascript/section01/solution17.js
@@ -1,4 +1,23 @@
 /*
+중복단어제거
 
+N개의 문자열이 입력되면 중복된 문자열은 제거하고 출력하는 프로그램 작성
+
+input : good, time, good, time, study
+output : good, time, study
 */
 
+let str = ["good", "time", "good", "time", "study"];
+console.log(`result: ${solution(str)}`);
+
+function solution(str) {
+    let answer;
+    // filter() : 고차함수로서 콜백함수가 참을 리턴한 value만 필터링
+    answer = str.filter((v, i) => {
+        // console.log(v, i);
+        // if(str.indexOf(v) === i) return true;
+        return str.indexOf(v) === i;
+    });
+
+    return answer;
+}
