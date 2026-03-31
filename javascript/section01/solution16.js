3aa079bf80c625f390b3e255bf11a69eda0e9b2a
MYEONGJU\mjk04 <kmj042894@gmail.com>
2026-04-01 06:59:38 +0900

    js study 20260401 1 update

--- javascript/section01/solution16.js
+++ javascript/section01/solution16.js
@@ -7,9 +7,11 @@ input : ksekkset
 output : kset
 */
 
-console.log(`result : ${solution("ksekkset")}`);
+console.log(`result1 : ${solution1("ksekkset")}`);
+console.log(`result2 : ${solution2("ksekkset")}`);
 
-function solution(str) {
+// 중복문자제거
+function solution1(str) {
     let answer = "";
 
     //indexOf()
@@ -19,6 +21,19 @@ function solution(str) {
         if(i === str.indexOf(str[i])) {
             answer += str[i];
         }
+    }
+
+    return answer;
+}
+
+// 특정문자의 개수
+function solution2(str) {
+    let answer = 0;
+    let pos = str.indexOf('k');
+    
+    while(pos !== -1) {
+        answer++;
+        pos = str.indexOf('k', pos + 1);
     }
 
     return answer;
