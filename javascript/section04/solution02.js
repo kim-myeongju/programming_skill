24bda8f7e89d3ed872a657ac787726f26a2bc70e
MYEONGJU\mjk04 <kmj042894@gmail.com>
2026-05-04 07:21:27 +0900

    js study 20260504 2

--- javascript/section04/solution02.js
+++ javascript/section04/solution02.js
@@ -10,11 +10,13 @@ output : 23, 2, 73, 2, 3
 
 let nums = [32, 55, 62, 20, 250, 370, 200, 30, 100];
 console.log(`result01 : ${solution01(nums)}`);
+console.log(`result02 : ${solution02(nums)}`);
 
+// 소수판별함수
 function isPrime(num) {
     if(num === 1) return false;
     
-    // 소수판별법 (0, 1, 자기자신 제외한 나머지 숫자로 나누어 떨어지는 수가 있으면 소수가 아님)
+    // 소수판별법 : 0, 1, 자기자신 제외한 나머지 숫자로 나누어 떨어지는 수가 있으면 소수가 아님
     // 절반 또는 제곱근까지만 검사가능
     for(let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
         if(num % i === 0) return false;
@@ -36,6 +38,18 @@ function solution01(nums) {
             num = parseInt(num / 10);
         }
         
+        if(isPrime(res)) answer.push(res);
+    }
+
+    return answer;
+}
+
+function solution02(nums) {
+    let answer = [];
+
+    for(let num of nums) {
+        // 내장함수 사용해서 숫자 뒤집기
+        let res = Number(num.toString().split('').reverse().join(''));
         if(isPrime(res)) answer.push(res);
     }
 
