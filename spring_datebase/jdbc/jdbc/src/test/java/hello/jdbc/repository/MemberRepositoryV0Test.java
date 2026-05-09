48df53ace436b7c109361932345d01cc0fa225ed
MYEONGJU\mjk04 <kmj042894@gmail.com>
2026-05-10 08:22:58 +0900

    spring db study 20260510

---
+++ spring_datebase/jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,19 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class MemberRepositoryV0Test {
+
+    MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+    @Test
+    void crud() throws SQLException {
+        // save
+        Member member = new Member("memberV1", 10000);
+        repository.save(member);
+    }
+
+}
